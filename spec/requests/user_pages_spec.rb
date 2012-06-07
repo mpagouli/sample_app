f339a4914aa8cd6b9608b225786e694b8a0edd44
Nina <mpagouli@gmail.com>
2012-06-07 17:06:00 +0300

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,7 +32,9 @@ describe "UserPages" do
     end
   end
 
-
+  describe "after saving the user" do    
+    it { should have_link('Sign out') }
+  end
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
@@ -41,5 +43,7 @@ describe "UserPages" do
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
   end
+
+
 
 end
